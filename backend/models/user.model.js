a7110ee1be2df11411e886f46366793cc025517d
Harith Kp <harithpradeepan@gmail.com>
2025-08-03 15:46:51 +0530

    1-user routes and controllers

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -48,6 +48,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    bio: {
+      type: String,
+      default: "",
+    },
   },
   { timestamps: true }
 );
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
-import connectMongoDB from "./db/connectMongoDB.js";
-import cookieParser from "cookie-parser";
-dotenv.config();
-const app = express();
+import userRoutes from "./routes/user.routes.js";
 
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -15,6 +24,7 @@ app.use(express.urlencoded({ extended: true })); //to parse form data in req.bod
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
