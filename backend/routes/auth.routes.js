f8331e91934a8b56407d42f7638382c64562976f
Harith Kp <harithpradeepan@gmail.com>
2025-08-03 13:26:14 +0530

    1-auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { logout, signup, login } from "../controllers/auth.controller.js";
+import {
+  logout,
+  signup,
+  login,
+  getMe,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 const router = express.Router();
 
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
+
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 import express from "express";
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/api/auth", authRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //to parse form data in req.body urlencoded
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
